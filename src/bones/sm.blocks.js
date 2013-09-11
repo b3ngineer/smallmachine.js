780e806646cdfcd82ccda0eca79c7eae91d831d9
Ben <ben.harris@netscape.com>
2013-09-10 19:22:48 -0600

    addMessageType combines same named models
    If a named model already exists, then adding a new prototype merges the
    behavior.

--- src/bones/sm.blocks.js
+++ src/bones/sm.blocks.js
@@ -1,41 +1,41 @@
 ;(function(sm, $, raphael) {
 	'use strict';
 
-    var Guid = {
-        generate : function() {
-            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-                var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-                return v.toString(16);
-            });
-        }
-    };
+	var Guid = {
+		generate : function() {
+			return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+				var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+				return v.toString(16);
+			});
+		}
+	};
 
-    sm.addMessageType('Block', function(params, $text) {
-        if (typeof params.getType === 'function' && params.getType() === '[object KeyPressEvent]') {
-            params.id = Guid.generate();
-            if (typeof $text !== 'undefined') {
-                var position = $text.position();
-                params.x = position.left;
-                params.y = position.top;
-                params.width = $text.width();
-                params.height = $text.height();
-                params.text = $text.val();
-            }
-        }
-        this.id = params.id;
-        this.x = params.x;
-        this.y = params.y;
-        this.height = params.height;
-        this.width = params.width;
-        this.text = params.text || '';
-        return this;
-    });
+	sm.addMessageType('Block', function(params, $text) {
+		if (typeof params.getType === 'function' && params.getType() === '[object KeyPressEvent]') {
+			params.id = Guid.generate();
+			if (typeof $text !== 'undefined') {
+				var position = $text.position();
+				params.x = position.left;
+				params.y = position.top;
+				params.width = $text.width();
+				params.height = $text.height();
+				params.text = $text.val();
+			}
+		}
+		this.id = params.id;
+		this.x = params.x;
+		this.y = params.y;
+		this.height = params.height;
+		this.width = params.width;
+		this.text = params.text || '';
+		return this;
+	});
 
-    sm.addMessageType('CanvasEvent', function(e, context, $text) {
+	sm.addMessageType('CanvasEvent', function(e, context, $text) {
 		this.x = e.offsetX || e.pageX;
 		this.y = e.offsetY || e.offsetY;
-        this.context = context;
-        this.$text = $text;
+		this.context = context;
+		this.$text = $text;
 		return this;
 	});
 
@@ -44,16 +44,16 @@
 			var width = $(window).width();
 			var height = $(window).height();
 			var paper = raphael('paper', width, height);
-            var $text = $('<textarea id="text" style="position:absolute;"></textarea>').hide();
-            $('body').append($text);
-            $text.keypress(function(e) {
-                if (e.keyCode == 13) {
-                    e.getType = function() { return '[object KeyPressEvent]'; }
-                    var block = new sm.types.Block(e, $text);
-                    sm.system.insert.hook(block, paper);
-                    $text.hide();
-                }
-            });
+			var $text = $('<textarea id="text" style="position:absolute;"></textarea>').hide();
+			$('body').append($text);
+			$text.keypress(function(e) {
+				if (e.keyCode == 13) {
+					e.getType = function() { return '[object KeyPressEvent]'; }
+					var block = new sm.types.Block(e, $text);
+					sm.system.insert.hook(block, paper);
+					$text.hide();
+				}
+			});
 			$('#paper').click(function(e) { sm.user.click.publish(new sm.types.CanvasEvent(e, paper, $text)); });
 			sm.system.reactsTo.click.subscribe({ update : function(message) { return defaultNewText; } }); 
 			sm.system.insert.subscribe({ update : function(message) { return defaultBlocks; } });
@@ -66,27 +66,27 @@
 
 	var defaultNewText = {
 		update : function(message) {
-            if (!message instanceof sm.types.CanvasEvent) {
-                return false;
-            }
-            message.$text.css({top:message.y + 'px',left:message.x + 'px'}).show().select();
+			if (!message instanceof sm.types.CanvasEvent) {
+				return false;
+			}
+			message.$text.css({top:message.y + 'px',left:message.x + 'px'}).show().select();
 		}
 	}
 
 	var defaultBlocks = {
 		update : function(message) {
-            if (!message instanceof sm.types.Hook) {
-                return false;
-            }
+			if (!message instanceof sm.types.Hook) {
+				return false;
+			}
 			var block = message.target;
 			var paper = message.context;
 			var rect = paper.rect(block.x, block.y, block.width, block.height, 3);
 			rect.node.id = block.id;
-            // TODO: figure out how to position the text vertically
-            var text = paper.text(block.x + 5, block.y + 10, block.text).attr({
-                'text-anchor': 'start',
-                'font-size': 15
-            });
+			// TODO: figure out how to position the text vertically
+			var text = paper.text(block.x + 5, block.y + 10, block.text).attr({
+				'text-anchor': 'start',
+				'font-size': 15
+			});
 		}
 	};
 
--- src/sm.core.js
+++ src/sm.core.js
@@ -3,63 +3,68 @@
 
 	Function.prototype.alsoBehavesLike = function(something) {
 		for (var p in something.prototype) {
-            // don't copy "private" members
+			// don't copy "private" members
 			if (p.indexOf('_') == 0) {
 				continue;
 			}
-            if (typeof this.prototype[p] !== 'undefined') {
-                continue;
-            }
+			// don't overwrite existing members
+			if (typeof this.prototype[p] !== 'undefined') {
+				continue;
+			}
 			this.prototype[p] = something.prototype[p];
 		}
 		for (var p in something) {
 			if (!something.hasOwnProperty(p)) {
 				continue;
 			}
-            // don't copy "private" members
 			if (p.indexOf('_') == 0) {
 				continue;
 			}
-            if (typeof this.prototype[p] !== 'undefined') {
-                continue;
-            }
+			if (typeof this.prototype[p] !== 'undefined') {
+				continue;
+			}
 			this.prototype[p] = something[p];
 		}
 	}
 
 	core.CONCEPT = 'concept';
 	core.RELATIONSHIP = 'relationship';
-    core.types = {};
+	core.types = {};
 
-    core.addMessageType = function(name, classDef) {
-        if (typeof classDef !== 'function') {
-            throw 'Cannot create a message type without a constructor (function)';
-        }
-        core.types[name] = classDef;
-        core.types[name].prototype.getType = function() {
-            return '[object ' + name + ']';
-        };
-    };
+	core.addMessageType = function(name, classDef) {
+		if (typeof classDef !== 'function') {
+			throw 'Cannot create a message type without a constructor (function)';
+		}
+		if (typeof core.types[name] !== 'undefined') {
+			core.types[name].alsoBehavesLike(classDef);
+		}
+		else {
+			core.types[name] = classDef;
+			core.types[name].prototype.getType = function() {
+				return '[object ' + name + ']';
+			};
+		}
+	};
 
-    var AsyncResult = function(channel) {
-        this._channel = channel;
-        return this;
-    };
+	var AsyncResult = function(channel) {
+		this._channel = channel;
+		return this;
+	};
 
-    AsyncResult.prototype.publish = function(message, recipients) {
-        this._channel.publish(message, recipients);
-        return this;
-    };
+	AsyncResult.prototype.publish = function(message, recipients) {
+		this._channel.publish(message, recipients);
+		return this;
+	};
 
-    core.addMessageType('AsyncResult', AsyncResult);
+	core.addMessageType('AsyncResult', AsyncResult);
 
 	var Channel = function() {
 		return this;
 	};
 
-    Channel.prototype.forward = function() {
-        throw new Error('Missing implementation of forward'); 
-    };
+	Channel.prototype.forward = function() {
+		throw new Error('Missing implementation of forward'); 
+	};
 
 	Channel.prototype.subscribe = function(subscriber) {
 		if (typeof this._subscribers === 'undefined') {
@@ -75,139 +80,139 @@
 		return this;
 	};
 
-    var notify = function(message, subscribers) {
-        var authoritative = false,
-            delegates = [],
-            deference = [];
-        for (var i = 0; i < subscribers.length; i++) {
-            var response = subscribers[i].update(message);
-            if (response === true) {
-                authoritative = true;
-            }
-            else if (typeof response === 'function') {
-                deference.push(response);
-            }
-            else if (typeof response === 'object' && typeof response.update === 'function') {
-                delegates.push(response.update);
-            }
-        }
-        if (authoritative) {
-            for (var i = 0; i < deference.length; i++) {
-                deference[i](message);
-            }
-        }
-        else {
-            for (var i = 0; i < delegates.length; i++) {
-                delegates[i](message);
-            }
-        }
-    };
+	var notify = function(message, subscribers) {
+		var authoritative = false,
+			delegates = [],
+			deference = [];
+		for (var i = 0; i < subscribers.length; i++) {
+			var response = subscribers[i].update(message);
+			if (response === true) {
+				authoritative = true;
+			}
+			else if (typeof response === 'function') {
+				deference.push(response);
+			}
+			else if (typeof response === 'object' && typeof response.update === 'function') {
+				delegates.push(response.update);
+			}
+		}
+		if (authoritative) {
+			for (var i = 0; i < deference.length; i++) {
+				deference[i](message);
+			}
+		}
+		else {
+			for (var i = 0; i < delegates.length; i++) {
+				delegates[i](message);
+			}
+		}
+	};
 
 	Channel.prototype.publish = function(message, recipients) {
 		var recipients = recipients || { };
-        if (typeof message === 'function') {
-            var newResult = message(new core.types.AsyncResult(this));
-            message = newResult;
-            if (typeof message !== 'undefined' &&
-                typeof message.getType === 'function' &&
-                message.getType() === '[object AsyncResult]') {
-                return this;
-            }
-        }
+		if (typeof message === 'function') {
+			var newResult = message(new core.types.AsyncResult(this));
+			message = newResult;
+			if (typeof message !== 'undefined' &&
+				typeof message.getType === 'function' &&
+				message.getType() === '[object AsyncResult]') {
+				return this;
+			}
+		}
 		if (typeof this._subscribers === 'undefined') {
 			this.forward(message, recipients);
 			return this;
 		}
-        notify(message, this._subscribers);
+		notify(message, this._subscribers);
 		this.forward(message, recipients);
 		return this;
 	};
 
-    Channel.prototype.addHelper = function(name, helper) {
+	Channel.prototype.addHelper = function(name, helper) {
 		var method = Channel.prototype.publish;
-        var context = this; // context becomes static to the channel adding the helper
-        this[name] = function() {
-            var args = [];
-            for (var i = 0; i < arguments.length; i++) {
-                args.push(arguments[i]);
-            }
+		var context = this; // context becomes static to the channel adding the helper
+		this[name] = function() {
+			var args = [];
+			for (var i = 0; i < arguments.length; i++) {
+				args.push(arguments[i]);
+			}
 			return method.apply(context, [function(asyncResult) {
-                args.push(asyncResult);
-                return helper.apply(context, args);
-            }]);
-        };
-        return this;
-    };
+				args.push(asyncResult);
+				return helper.apply(context, args);
+			}]);
+		};
+		return this;
+	};
 
 	var Rules = function() {
 		return this;
 	};
 
-    Rules.prototype.isA = function(o) {
-        throw new Error('Missing implementation for isA');	
-    };
-
-    Rules.prototype.hasRange = function(o) {
-        throw new Error('Missing implementation for hasRange');	
-    };
-
-    Rules.prototype.hasDomain = function(o) { 
-        throw new Error('Missing implementation for hasDomain');	
-    };
-
-    Rules.prototype.relatesTo = function(p, o) {
-        throw new Error('Missing implementation for relatesTo');	
-    };
-
-	core.Term = function(value) {
-        if (typeof value._value !== 'undefined' && typeof value._type !== 'undefined') {
-            this._value = value._value;
-            this._type = value._type;
-        }
-        else {
-            this._value = value;
-            this._type = null;
-        }
-        return this;
+	Rules.prototype.isA = function(o) {
+		throw new Error('Missing implementation for isA');	
 	};
 
-    /* EVERY term is an immediate child of the core */
+	Rules.prototype.hasRange = function(o) {
+		throw new Error('Missing implementation for hasRange');	
+	};
+
+	Rules.prototype.hasDomain = function(o) { 
+		throw new Error('Missing implementation for hasDomain');	
+	};
+
+	Rules.prototype.relatesTo = function(p, o) {
+		throw new Error('Missing implementation for relatesTo');	
+	};
+
+	core.Term = function(value) {
+		if (typeof value._value !== 'undefined' && typeof value._type !== 'undefined') {
+			this._value = value._value;
+			this._type = value._type;
+		}
+		else {
+			this._value = value;
+			this._type = null;
+		}
+		return this;
+	};
+
+	/* EVERY term is an immediate child of the core */
 	core.add = function(Term) {
 		core[Term._value] = Term;
 		return Term;
 	};
 
-    core.Term.prototype.forward = function(message, recipients) {
-        if (this._value != null) {
-            recipients[this._value] = true;
-        }
-        for (var property in this) {
-            if (!this.hasOwnProperty(property)) {
-                continue;
-            }
-            if (typeof this[property] === 'function' || property.indexOf('_') === 0) {
-                continue;
-            }
-            if (recipients[property] === true) {
-                continue;
-            }
-            if (typeof this[property].publish === 'function') {
-                recipients[property] = true;
-                this[property].publish(message, recipients);
-            }
-        }
-        if (typeof this._relatesTo !== 'undefined') {
-            for (var i = 0; i < this._relatesTo.length; i++) {
-                if (typeof this._relatesTo[i]._subscribers === 'undefined') {
-                    continue;
-                }
-                // Relationships do *not* continue to notify their object properties when being forwarded to
-                notify(message, this._relatesTo[i]._subscribers);
-            }
-        }
-    };
+	core.Term.prototype.forward = function(message, recipients) {
+		if (this._value != null) {
+			recipients[this._value] = true;
+		}
+		for (var property in this) {
+			if (!this.hasOwnProperty(property)) {
+				continue;
+			}
+			if (typeof this[property] === 'function' || property.indexOf('_') === 0) {
+				continue;
+			}
+			if (recipients[property] === true) {
+				continue;
+			}
+			if (typeof this[property].publish === 'function') {
+				recipients[property] = true;
+				this[property].publish(message, recipients);
+			}
+		}
+		if (typeof this._relatesTo !== 'undefined') {
+			for (var i = 0; i < this._relatesTo.length; i++) {
+				if (typeof this._relatesTo[i]._subscribers === 'undefined') {
+					continue;
+				}
+				// Relationships do *not* continue to notify their object properties when being forwarded to
+				notify(message, this._relatesTo[i]._subscribers);
+			}
+		}
+	};
 
-    /* relatesTo scopes cross-cutting concerns by implicitly grouping concepts */
+	/* relatesTo scopes cross-cutting concerns by implicitly grouping concepts */
 	core.Term.prototype.relatesTo = function(TermA, TermB) {
 		if (TermA._type === core.CONCEPT) {
 			throw new Error('Cannot define a relationship with a concept type: ' + TermA._value);
@@ -215,29 +220,25 @@
 		else if (TermA._type === null) {
 			TermA._type = core.RELATIONSHIP;
 		}
-
 		if (TermB._type === core.RELATIONSHIP) {
 			throw new Error('Cannot create a relationship with a relationship type: ' + TermB._value);
 		}
 		else if (TermB._type === null) {
 			TermB._type = core.CONCEPT;
 		}
-
 		if (this._type === core.RELATIONSHIP) {
 			throw new Error('Cannot create a relationship from a relationship type: ' + this._value);
 		}
 		else if (this._type === null) {
 			this._type = core.CONCEPT;
 		}
-
 		core[this._value][TermA._value] = new core.Term(TermA); // isolate relationship scope
 		core[this._value][TermA._value][TermB._value] = TermB; // put object property in scope
-        core[this._value][TermB._value] = TermB; // sugar for including property as child
-        if (typeof TermB._relatesTo === 'undefined') {
-            TermB._relatesTo = [];
-        };
-        TermB._relatesTo.push(core[this._value][TermA._value]);
-
+		core[this._value][TermB._value] = TermB; // sugar for including property as child
+		if (typeof TermB._relatesTo === 'undefined') {
+			TermB._relatesTo = [];
+		};
+		TermB._relatesTo.push(core[this._value][TermA._value]);
 		for (var field in TermB) {
 			if (!TermB.hasOwnProperty(field)) {
 				continue;
@@ -249,11 +250,11 @@
 				continue;
 			}
 			core[this._value][TermA._value][field] = TermB[field]; // link subclasses in scope
-            core[this._value][field] = TermB[field]; // sugar for linking subclasses as children
-            if (typeof TermB[field]._relatesTo === 'undefined') {
-                TermB[field]._relatesTo = [];
-            };
-            TermB[field]._relatesTo.push(core[this._value][TermA._value]);
+			core[this._value][field] = TermB[field]; // sugar for linking subclasses as children
+			if (typeof TermB[field]._relatesTo === 'undefined') {
+				TermB[field]._relatesTo = [];
+			};
+			TermB[field]._relatesTo.push(core[this._value][TermA._value]);
 		};
 		return this;
 	};
@@ -291,12 +292,11 @@
 			this._type = core.RELATIONSHIP;
 		}
 		core[this._value][Term._value] = Term;
-        
-        if (typeof Term._relatesTo === 'undefined') {
-            Term._relatesTo = [];
-        }
-        Term._relatesTo.push(this);
-
+		
+		if (typeof Term._relatesTo === 'undefined') {
+			Term._relatesTo = [];
+		}
+		Term._relatesTo.push(this);
 		for (var field in Term) {
 			if (!Term.hasOwnProperty(field)) {
 				continue;
@@ -308,17 +308,17 @@
 				continue;
 			}
 			core[this._value][Term[field]._value] = Term[field];
-            if (typeof Term[field]._relatesTo === 'undefined') {
-                Term[field]._relatesTo = [];
-            }
-            Term[field]._relatesTo.push(this);
+			if (typeof Term[field]._relatesTo === 'undefined') {
+				Term[field]._relatesTo = [];
+			}
+			Term[field]._relatesTo.push(this);
 		};
 		return this;
 	};
 
 	/* object property domain universally relates concepts upstream from an edge */
 	core.Term.prototype.hasDomain = function(Term) {
-		if (Term._type === core.RELATIONSIP) {
+	    if (Term._type === core.RELATIONSIP) {
 			throw new Error('Cannot apply hasRange to a relationship type: ' + Term._value);
 		}
 		else if (Term._type === null) {
@@ -335,7 +335,7 @@
 	};
 
 	core.Term.alsoBehavesLike(Channel);
-	core.Term.alsoBehavesLike(Rules);
+    core.Term.alsoBehavesLike(Rules);
 
 	return core;
 }(smallmachine || {}));
