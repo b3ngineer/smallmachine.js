5434db215950cdeb6ddb3d9a1adf9ab709f9fcd9
b3ngineer <ben.harris@netscape.com>
2013-09-15 11:24:59 -0600

    updated the readme to reflect the overhaul

--- README.md
+++ README.md
@@ -2,80 +2,62 @@
 
 ## What is it? ##
 In a sentence, *smallmachine.js* is a 
-"*semantic messaging strategy*".  It can be used standalone to wire components into an application layer&mdash;or it can be used to enhance the event model in an existing framework or toolkit.
+"semantic model strategy".  It can be used to build domain models using rules based inference.  Additionally, business logic can be attached to domain models using mixins and helpers.  The *smallmachine.js* project includes an example ontology to simplify the problem domain of event modeling (for the use of a publisher subscriber pattern).
 
-```js
-(function(sm){
-	// myHandler wants to be updated with all user click messages
-	sm.user.click.subscribe(myHandler);
+## The Example Ontology: Channels ##
 
-	// myFirstMessage is heard by myHandler
-	sm.user.click.publish(myFirstMessage);
-
-	// mySecondMessage is also heard by myHandler,
-	// ...because user.click is an action
-	sm.action.publish(mySecondMessage);
-}(smallmachine));
-```
-
-### The problem domain ###
-Event models are typically lists of strings, globally defined constants, or properties on a particular object.  Knowledge of the pinpointed events in the model is a requirement of components and plugins that are bound to the application layer.  One problem with such a model is that coupling can become strong when event models are text-based, or when event models are extended to use component specific events.  An inherent complication with an event "list" model occurs when events need to be sequenced or prioritized so that components can respond in order.
-
-## What does it include? ##
-
-### An ontology, complete with inference  ###
+### What does it include? ###
 Just as an ontology helps organize relationships between related *things* in a semantic data model, *channels* can be organized in a fashion that describes their purpose with widespread clarity.  *Smallmachine.js* channels are organized as terms that can be either concepts or relationships (nodes and edges in a graph).  The *smallmachine.js* model is extended via **rules** rather than strings&mdash;the usage of which **infers additional relationships between channels**.  Event emissions propagate along intuitive paths through the ontology&mdash;letting component and plugin authors remain (generally) naive of externally defined custom channels... even while handling any externally defined channels fitted into the *smallmachine.js* ontology.
 
 ```js
 (function(sm) {
-	// demo of rules based model extension
-	sm.add(new ontology.Term('thing'));
-	sm.add(new ontology.Term('user'));
-	sm.add(new ontology.Term('performs'));
-	sm.add(new ontology.Term('action'));
-	sm.user.isA(ontology.thing);
-	sm.action.isA(ontology.thing);
-	sm.performs.hasRange(ontology.action);
-	sm.user.relatesTo(sm.performs, sm.action);
+	// create empty ontology ontology
+	var channels = sm('my channels example');
+
+	// add patterned behaviors to the model
+	channels.Term.alsoBehavesLike(sm.Channel);
+
+	// build the channels ontology
+	sm.defaultChannels(channels);
 }(smallmachine));
 ```
 
 ### A succinct syntax ###
-The hierarchy of the *smallmachine.js* model is integrated with an asynchronous publisher/subscriber pattern.  That is to say, every property in the object hierarchy is a semantically named channel that supports ```publish()``` and ```subscribe()``` methods.  All behaviors are derived from those two methods.  
+The hierarchy of the *smallmachine.js* channels model is integrated with an asynchronous publisher/subscriber pattern.  That is to say, every property in the object hierarchy is a semantically named channel that supports ```publish()``` and ```subscribe()``` methods.  All behaviors are derived from those two methods.  
 
 ```js
-(function(sm) {
-	// the core ontology defines 'user' as a Term
+(function(channels) {
+	// the example channels ontology defines 'user' as a Term
 
 	// all Terms are channels
-	sm.user.subscribe(...);
-	sm.user.publish(...);
+	channels.user.subscribe(...);
+	channels.user.publish(...);
 
-	// the core ontology defines 'action' as being performed by 'users'
-	sm.user.performs.action.click.subscribe(...);
-	sm.user.performs.action.click.publish(...);
+	// the channels ontology defines 'action' as being performed by 'users'
+	channels.user.performs.action.click.subscribe(...);
+	channels.user.performs.action.click.publish(...);
 
-	// the core ontology defines 'action' in the range of 'performs'
+	// the channels ontology defines 'action' in the range of 'performs'
 	// ... 'performs' is a relationship between 'users' and 'actions'
 	// ... 'click' is a subclass of 'action'
 	// ... therefore the syntax allows for user.click to be a valid channel
-	sm.user.click.subscribe(...);
-	sm.user.click.publish(...);
-}(smallmachine));
+	channels.user.click.subscribe(...);
+	channels.user.click.publish(...);
+}(ontology));
 ```
 
 Additionally, helpers are supplied on specific channels&mdash;as partially applied methods to ```publish()```&mdash;as a convenience for generating common message types.
 
 ```js
-(function(sm) {
+(function(channels) {
 	// publishes the data returned from the supplied URL
 	// ... to the system.initialize channel
-	sm.system.intialize.json('http://my.url.com/rest/api/data');
-}(smallmachine));
+	channels.system.intialize.json('http://my.url.com/rest/api/data');
+}(ontology));
 ```
 
 ### A notification lifecycle ###
-Subscribers remain relatively anonymous to other subscribers, thereby making event prioritization a tricky business.  To avoid the need for subscribers to explicitly arrange themselves in sequence, *smallmachine.js* incorporates a notification lifecycle that allows for deferred and conditional application behaviors.  Subscribers determine if they have handled the incoming message authoritatively or not.  They can defer execution until all subscribers have received message updates, and they can even abstain from acting unless no other subscribers declared authority.
+In the example domain model for channels, subscribers remain relatively anonymous to other subscribers, thereby making event prioritization a tricky business.  To avoid the need for subscribers to explicitly arrange themselves in sequence, *smallmachine.js* incorporates a notification lifecycle that allows for deferred and conditional application behaviors.  Subscribers determine if they have handled the incoming message authoritatively or not.  They can defer execution until all subscribers have received message updates, and they can even abstain from acting unless no other subscribers declared authority.
 
 Subscribers must implement the method ```update(message)```&mdash;which can return the following types (or nothing at all):
 
@@ -139,7 +121,7 @@ Subscribers must implement the method ```update(message)```&mdash;which can retu
 ```
 
 ### A strategy for reusing application layers ###
-Application interactions can be intuitively bound to the *smallmachine.js* ontology in a way that creates a larger base of reusable components...and also reusable application layers.  By supplying only-or-predominantly *default* behaviors through the notification lifecycle&mdash;generalized applications can supply the "bones" for multiple application domains.
+Application interactions can be intuitively bound to the channels ontology in a way that creates a larger base of reusable components...and also reusable application layers.  By supplying only-or-predominantly *default* behaviors through the notification lifecycle&mdash;generalized applications can supply the "bones" for multiple application domains.
 
 ## Laundry list ##
 * ~~MIT licensed~~
--- sm.library.js
+++ sm.library.js
@@ -5,7 +5,21 @@ var smallmachine = function(core) {
         continue
       }
       if(typeof this.prototype[p] !== "undefined") {
-        continue
+        var currentBehaviorIsVirtual = this.prototype[p].prototype.virtual;
+        var newBehaviorIsVirtual = something.prototype[p].prototype.virtual;
+        if(currentBehaviorIsVirtual && newBehaviorIsVirtual) {
+          throw new Error("Cannot mixin duplicate named virtual behaviors: " + p);
+        }else {
+          if(currentBehaviorIsVirtual) {
+            this.prototype[p] = something.prototype[p];
+            continue
+          }else {
+            if(newBehaviorIsVirtual) {
+              continue
+            }
+          }
+        }
+        throw new Error("Cannot mixin same-named behaviors: " + p);
       }
       this.prototype[p] = something.prototype[p]
     }
@@ -17,112 +31,22 @@ var smallmachine = function(core) {
         continue
       }
       if(typeof this.prototype[p] !== "undefined") {
-        continue
+        throw new Error("Cannot mixin same-named properties: " + p);
       }
       this.prototype[p] = something[p]
     }
   };
-  core.CONCEPT = "concept";
-  core.RELATIONSHIP = "relationship";
-  core.AsyncResult = function(channel) {
-    this._type = "AsyncResult";
-    this._channel = channel;
+  var Ontology = function(title) {
+    this._title = title;
     return this
   };
-  core.AsyncResult.prototype.publish = function(message, recipients) {
-    this._channel.publish(message, recipients)
-  };
-  var Channel = function() {
+  Ontology.prototype.add = function(value) {
+    var t = new this.Term(value, this);
+    this[t._value] = t;
     return this
   };
-  Channel.prototype.forward = function() {
-    throw new Error("Missing implementation of forward");
-  };
-  Channel.prototype.subscribe = function(subscriber) {
-    if(typeof this._subscribers === "undefined") {
-      this._subscribers = []
-    }
-    if(typeof subscriber === "function") {
-      this._subscribers.push({update:subscriber});
-      return this
-    }
-    this._subscribers.push(subscriber);
-    return this
-  };
-  var notify = function(message, subscribers) {
-    var authoritative = false, delegates = [], deference = [];
-    for(var i = 0;i < subscribers.length;i++) {
-      var response = subscribers[i].update(message);
-      if(response === true) {
-        authoritative = true
-      }else {
-        if(typeof response === "function") {
-          deference.push(response)
-        }else {
-          if(typeof response === "object" && typeof response.update === "function") {
-            delegates.push(response.update)
-          }
-        }
-      }
-    }
-    if(authoritative) {
-      for(var i = 0;i < deference.length;i++) {
-        deference[i](message)
-      }
-    }else {
-      for(var i = 0;i < delegates.length;i++) {
-        delegates[i](message)
-      }
-    }
-  };
-  Channel.prototype.publish = function(message, recipients) {
-    var recipients = recipients || {};
-    if(typeof message === "function") {
-      var newResult = message(new core.AsyncResult(this));
-      message = newResult;
-      if(typeof message !== "undefined" && typeof message._type !== "undefined" && message._type === "AsyncResult") {
-        return this
-      }
-    }
-    if(typeof this._subscribers === "undefined") {
-      this.forward(message, recipients);
-      return this
-    }
-    notify(message, this._subscribers);
-    this.forward(message, recipients);
-    return this
-  };
-  Channel.prototype.addHelper = function(name, helper) {
-    var method = Channel.prototype.publish;
-    var context = this;
-    this[name] = function() {
-      var args = [];
-      for(var i = 0;i < arguments.length;i++) {
-        args.push(arguments[i])
-      }
-      return method.apply(context, [function(asyncResult) {
-        args.push(asyncResult);
-        return helper.apply(context, args)
-      }])
-    };
-    return this
-  };
-  var Rules = function() {
-    return this
-  };
-  Rules.prototype.isA = function(o) {
-    throw new Error("Missing implementation for isA");
-  };
-  Rules.prototype.hasRange = function(o) {
-    throw new Error("Missing implementation for hasRange");
-  };
-  Rules.prototype.hasDomain = function(o) {
-    throw new Error("Missing implementation for hasDomain");
-  };
-  Rules.prototype.relatesTo = function(p, o) {
-    throw new Error("Missing implementation for relatesTo");
-  };
-  core.Term = function(value) {
+  Ontology.prototype.Term = function(value, ontology) {
+    this._ontology = ontology;
     if(typeof value._value !== "undefined" && typeof value._type !== "undefined") {
       this._value = value._value;
       this._type = value._type
@@ -132,39 +56,13 @@ var smallmachine = function(core) {
     }
     return this
   };
-  core.add = function(Term) {
-    core[Term._value] = Term;
-    return Term
+  var Rules = function(value, type, ontology) {
+    this._value = value;
+    this._type = type;
+    this._ontology = ontology;
+    return this
   };
-  core.Term.prototype.forward = function(message, recipients) {
-    if(this._value != null) {
-      recipients[this._value] = true
-    }
-    for(var property in this) {
-      if(!this.hasOwnProperty(property)) {
-        continue
-      }
-      if(typeof this[property] === "function" || property.indexOf("_") === 0) {
-        continue
-      }
-      if(recipients[property] === true) {
-        continue
-      }
-      if(typeof this[property].publish === "function") {
-        recipients[property] = true;
-        this[property].publish(message, recipients)
-      }
-    }
-    if(typeof this._relatesTo !== "undefined") {
-      for(var i = 0;i < this._relatesTo.length;i++) {
-        if(typeof this._relatesTo[i]._subscribers === "undefined") {
-          continue
-        }
-        notify(message, this._relatesTo[i]._subscribers)
-      }
-    }
-  };
-  core.Term.prototype.relatesTo = function(TermA, TermB) {
+  Rules.prototype.relatesTo = function(TermA, TermB) {
     if(TermA._type === core.CONCEPT) {
       throw new Error("Cannot define a relationship with a concept type: " + TermA._value);
     }else {
@@ -186,13 +84,13 @@ var smallmachine = function(core) {
         this._type = core.CONCEPT
       }
     }
-    core[this._value][TermA._value] = new core.Term(TermA);
-    core[this._value][TermA._value][TermB._value] = TermB;
-    core[this._value][TermB._value] = TermB;
+    this._ontology[this._value][TermA._value] = new this._ontology.Term(TermA);
+    this._ontology[this._value][TermA._value][TermB._value] = TermB;
+    this._ontology[this._value][TermB._value] = TermB;
     if(typeof TermB._relatesTo === "undefined") {
       TermB._relatesTo = []
     }
-    TermB._relatesTo.push(core[this._value][TermA._value]);
+    TermB._relatesTo.push(this._ontology[this._value][TermA._value]);
     for(var field in TermB) {
       if(!TermB.hasOwnProperty(field)) {
         continue
@@ -203,16 +101,16 @@ var smallmachine = function(core) {
       if(typeof TermB[field]._value === "undefined") {
         continue
       }
-      core[this._value][TermA._value][field] = TermB[field];
-      core[this._value][field] = TermB[field];
+      this._ontology[this._value][TermA._value][field] = TermB[field];
+      this._ontology[this._value][field] = TermB[field];
       if(typeof TermB[field]._relatesTo === "undefined") {
         TermB[field]._relatesTo = []
       }
-      TermB[field]._relatesTo.push(core[this._value][TermA._value])
+      TermB[field]._relatesTo.push(this._ontology[this._value][TermA._value])
     }
     return this
   };
-  core.Term.prototype.isA = function(Term) {
+  Rules.prototype.isA = function(Term) {
     if(Term._type === core.RELATIONSHIP) {
       throw new Error("Cannot apply isA to a relationship type: " + Term._value);
     }else {
@@ -227,10 +125,10 @@ var smallmachine = function(core) {
         this._type = core.CONCEPT
       }
     }
-    core[Term._value][this._value] = this;
+    this._ontology[Term._value][this._value] = this;
     return this
   };
-  core.Term.prototype.hasRange = function(Term) {
+  Rules.prototype.hasRange = function(Term) {
     if(Term._type === core.RELATIONSHIP) {
       throw new Error("Cannot apply hasRange to a relationship type: " + Term._value);
     }else {
@@ -245,7 +143,7 @@ var smallmachine = function(core) {
         this._type = core.RELATIONSHIP
       }
     }
-    core[this._value][Term._value] = Term;
+    this._ontology[this._value][Term._value] = Term;
     if(typeof Term._relatesTo === "undefined") {
       Term._relatesTo = []
     }
@@ -260,7 +158,7 @@ var smallmachine = function(core) {
       if(typeof Term[field]._value === "undefined") {
         continue
       }
-      core[this._value][Term[field]._value] = Term[field];
+      this._ontology[this._value][Term[field]._value] = Term[field];
       if(typeof Term[field]._relatesTo === "undefined") {
         Term[field]._relatesTo = []
       }
@@ -268,8 +166,8 @@ var smallmachine = function(core) {
     }
     return this
   };
-  core.Term.prototype.hasDomain = function(Term) {
-    if(Term._type === core.RELATIONSHIP) {
+  Rules.prototype.hasDomain = function(Term) {
+    if(Term._type === core.RELATIONSIP) {
       throw new Error("Cannot apply hasRange to a relationship type: " + Term._value);
     }else {
       if(Term._type === null) {
@@ -283,103 +181,42 @@ var smallmachine = function(core) {
         this._type = core.RELATIONSHIP
       }
     }
-    core[Term._value][this._value] = this;
+    this._ontology[Term._value][this._value] = this;
     return this
   };
-  core.Term.alsoBehavesLike(Channel);
-  core.Term.alsoBehavesLike(Rules);
+  Ontology.prototype.Term.alsoBehavesLike(Rules);
+  core = function(title) {
+    console.log(title);
+    return new Ontology(title)
+  };
+  core.CONCEPT = "concept";
+  core.RELATIONSHIP = "relationship";
+  core.types = {};
+  core.addMessageType = function(name, ctor) {
+    if(typeof ctor !== "function") {
+      throw new Error("Cannot create a message type without a constructor (function)");
+    }
+    if(typeof core.types[name] !== "undefined") {
+      core.types[name].alsoBehavesLike(ctor)
+    }else {
+      core.types[name] = ctor;
+      core.types[name].prototype.getType = function() {
+        return"[object " + name + "]"
+      };
+      core.types[name].prototype.ofType = function(type) {
+        return this.getType() == "[object " + type + "]"
+      }
+    }
+  };
+  var AsyncResult = function(channel) {
+    this._channel = channel;
+    return this
+  };
+  AsyncResult.prototype.publish = function(message, recipients) {
+    this._channel.publish(message, recipients);
+    return this
+  };
+  core.addMessageType("AsyncResult", AsyncResult);
   return core
 }(smallmachine || {});
-(function(ontology) {
-  ontology.add(new ontology.Term("thing"));
-  ontology.add(new ontology.Term("user"));
-  ontology.add(new ontology.Term("system"));
-  ontology.add(new ontology.Term("action"));
-  ontology.add(new ontology.Term("click"));
-  ontology.add(new ontology.Term("doubleClick"));
-  ontology.add(new ontology.Term("keyPress"));
-  ontology.add(new ontology.Term("task"));
-  ontology.add(new ontology.Term("initialize"));
-  ontology.add(new ontology.Term("insert"));
-  ontology.add(new ontology.Term("remove"));
-  ontology.add(new ontology.Term("get"));
-  ontology.add(new ontology.Term("set"));
-  ontology.add(new ontology.Term("messenger"));
-  ontology.add(new ontology.Term("success"));
-  ontology.add(new ontology.Term("error"));
-  ontology.add(new ontology.Term("performs"));
-  ontology.add(new ontology.Term("reactsTo"));
-  ontology.user.isA(ontology.thing);
-  ontology.system.isA(ontology.thing);
-  ontology.action.isA(ontology.thing);
-  ontology.task.isA(ontology.thing);
-  ontology.messenger.isA(ontology.thing);
-  ontology.click.isA(ontology.action);
-  ontology.doubleClick.isA(ontology.action);
-  ontology.keyPress.isA(ontology.action);
-  ontology.get.isA(ontology.task);
-  ontology.set.isA(ontology.task);
-  ontology.success.isA(ontology.messenger);
-  ontology.error.isA(ontology.messenger);
-  ontology.initialize.isA(ontology.task);
-  ontology.insert.isA(ontology.task);
-  ontology.remove.isA(ontology.task);
-  ontology.performs.hasRange(ontology.action).hasRange(ontology.task);
-  ontology.reactsTo.hasRange(ontology.action).hasDomain(ontology.system);
-  ontology.user.relatesTo(ontology.performs, ontology.action);
-  ontology.system.relatesTo(ontology.performs, ontology.task)
-})(smallmachine);
-(function(sm) {
-  var jsonHelper = function(url, asyncResult) {
-    if(typeof jQuery !== "undefined") {
-      try {
-        jQuery.ajax({dataType:"json", url:url, type:"GET", success:function(data, textStatus, jqxhr) {
-          sm.messenger.success.publish(textStatus);
-          asyncResult.publish(data)
-        }, error:function(jqxhr, textStatus, thrown) {
-          sm.messenger.error.publish(thrown)
-        }})
-      }catch(error) {
-        sm.messenger.error.publish(error)
-      }
-    }else {
-      var xhr = window.XMLHttpRequest ? new XMLHttpRequest : new ActiveXObject("MSXML2.XMLHTTP.3.0");
-      xhr.onprogress = function() {
-      };
-      xhr.onerror = {};
-      xhr.onreadystatechange = function() {
-        if(xhr.readyState < 4) {
-          return
-        }
-        if(xhr.status == 200) {
-          sm.messenger.success.publish(xhr.statusText);
-          asyncResult.publish(JSON.parse(xhr.responseText))
-        }else {
-          sm.messenger.error.publish(xhr.statusText)
-        }
-      };
-      xhr.open("GET", url, true);
-      xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-      xhr.setRequestHeader("Content-type", "application/json");
-      xhr.send(null)
-    }
-    return asyncResult
-  };
-  sm.system.initialize.addHelper("json", jsonHelper)
-})(smallmachine);
-(function(sm) {
-  function Hook(targetObject, contextObject) {
-    this.target = targetObject;
-    this.context = contextObject;
-    return this
-  }
-  Hook.prototype.getType = function() {
-    return"[object Hook]"
-  };
-  var hookHelper = function(targetObject, contextObject, asyncResult) {
-    var hookObject = new Hook(targetObject, contextObject);
-    return hookObject
-  };
-  sm.system.insert.addHelper("hook", hookHelper)
-})(smallmachine);
 
