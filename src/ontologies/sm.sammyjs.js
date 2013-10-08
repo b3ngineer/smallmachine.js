df1c2c576d378c86e02ef077861379ff9f9c286b
Ben <ben.harris@netscape.com>
2013-10-08 11:52:19 -0600

    added ontology for Sammy.js
    Initialize channel wired up to accept sm.type.Sammy objects to run
    applications that use the sammyjs lib.

--- src/ontologies/sm.sammyjs.js
+++ src/ontologies/sm.sammyjs.js
@@ -1,15 +1,39 @@
 ;(function(sm, sammy) {
 	var ontology = new sm.Ontology('sm.sammyjs');
 
+	ontology.addTerm('initialize');
+
 	var Sammy = function(id, callback) {
 		this._id = id;
 		this._callback = callback;
 		return this;
 	};
 
+	Sammy.prototype.getType = function() {
+		return '[object Sammy]';
+	};
+
+	Sammy.prototype.ofType = function(type) {
+		return type === 'Sammy' || (typeof type.getType === 'function' && type.getType() === this.getType());
+	};
+
 	sm.type.extendedBy(Sammy, 'Sammy');
+	
+	var sammyInitializerDelegate = {
+		update : function(message) {
+			if (typeof message.ofType === 'function' && message.ofType('Sammy')) {
+				return function(message) {
+					sammy('#' + message._id, message._callback).run();
+				};
+			}
+			return false;
+		}
+	};
 
 	var activator = function(model) {
+		model.initialize.subscribe({
+			update : function(message) { return sammyInitializerDelegate; }
+		});
 	};
 
 	ontology.registerActivator(activator, ['sm.channels']);
