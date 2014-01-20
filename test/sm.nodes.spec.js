33ec5ca783c20f0965b0ff1a067c2864b2b24543
Ben <ben.harris@netscape.com>
2014-01-20 07:10:45 -0700

    removed all non-core functionality
    All of the supporting functionality has been moved to separate projects.

--- test/sm.nodes.spec.js
+++
@@ -1,5 +0,0 @@
-describe('ontologies/sm.nodes', function() {
-    it('should allow calling publish directly', function() {
-        expect(typeof target.system.initialize.publish).toBe('function');
-    });
-});
--- tools/build.sh
+++ tools/build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-java -jar ../lib/compiler.jar --compilation_level=SIMPLE_OPTIMIZATIONS --js=../src/sm.core.js --js=../src/ontologies/sm.properties.js --js_output_file=../sm.library.min.js
+java -jar ../lib/compiler.jar --compilation_level=SIMPLE_OPTIMIZATIONS --js=../src/sm.core.js --js=../src/sm.properties.js --js_output_file=../sm.library.min.js
 
-java -jar ../lib/compiler.jar --compilation_level=WHITESPACE_ONLY --formatting=pretty_print --js=../src/sm.core.js --js=../src/ontologies/sm.properties.js --js_output_file=../sm.library.js
+java -jar ../lib/compiler.jar --compilation_level=WHITESPACE_ONLY --formatting=pretty_print --js=../src/sm.core.js --js=../src/sm.properties.js --js_output_file=../sm.library.js
 
-java -jar ../lib/compiler.jar --compilation_level=SIMPLE_OPTIMIZATIONS --js=../src/sm.core.js --js=../src/ontologies/sm.properties.js --js=../src/ontologies/sm.channels.js --js=../src/behaviors/sm.Channel.js --js=../src/behaviors/sm.Extension.js --js=../src/ontologies/sm.nodes.js --js=../src/ontologies/sm.flowchart.js --js=../src/ontologies/sm.raphaeljs.js --js=../src/ontologies/sm.sammyjs.js --js_output_file=../sm.library-all.min.js
